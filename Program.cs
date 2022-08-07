952b189d9ddc8cc8c5004bad149c12faaae5c707
bhumika1111 <bhoyarpatil@434>
2022-08-07 17:27:24 +0530

    * Greeting Message *

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace EmpWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********Welcome To Employee Wage Computation Program************");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Press 0 To 'Exit' **");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Press 1 To  Check Employee Attendance**");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Press 2 To  Calculate Employee Daily Wage.**");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Press 3 To Check Employee Part Time Or Full Time Working And Also Calculate Wages**");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Press 4 To  Check Employee Daily Wage Cmputation Using Switch Case**");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Press 5 To Calculate Employee Wage of A Month**");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Press 6 To Calculate Employee Wage Till Condition MaxHrs:100,MaxDays:20**");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Press 7 To Calculate Employee Wage For Multiple Company**");
+            Console.WriteLine("**:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::**");
+           
+            Console.WriteLine("** Enter Your Choice: ***");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine("***********Output For Employee Is Present Or Absent**************");
+                    //Creating object/Instance Of Class EmployeePresence...
+                    EmployeePresence objPresence = new EmployeePresence();
+
+                    //Calling The Method/Function..
+                    objPresence.CheckEmpPresence();
+                    break;
+
+                case 2:
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine("***********Output For Employee Daily Wage**************");
+                    EmployeeDailyWage objEmpDailyWage = new EmployeeDailyWage();
+                    objEmpDailyWage.CheckEmpDailyWage();
+                    break;
+
+                case 3:
+
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine("********Output For Employee Wage After Adding Part Time.*********** ");
+                    EmployeeWageForPartTime objPartOrFullTime = new EmployeeWageForPartTime();
+                    objPartOrFullTime.CheckEmpWagePartTime();
+                    break;
+
+                case 4:
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine("*********Output For Employee Wage Using Switch Case************");
+                    EmployeeWageSwitchCase objSwitchCaseEmpWage = new EmployeeWageSwitchCase();
+                    objSwitchCaseEmpWage.CheckEmpAttendance();
+                    break;
+
+                case 5:
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine("********Output For Employee Wage For Month*************");
+                    EmployeeWageForMonth objEmpWageForMonth = new EmployeeWageForMonth();
+                    objEmpWageForMonth.CheckEmpWageForMonth();
+                    break;
+
+                case 6:
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine("********Output For Employee Total Working Hrs Or Days In Month***********");
+                    EmployeeWageHrsInMonth objEmpWageHrsInMonth = new EmployeeWageHrsInMonth();
+                    objEmpWageHrsInMonth.CheckEmpWageHrsOrDayInMonth();
+                    break;
+
+                case 7:
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine("********Output For Employee Wage For Multiple Company**************");
+                    EmpWageForMulCompany objEmpWageForMulCompany = new EmpWageForMulCompany();
+                    objEmpWageForMulCompany.computeEmpWage("Dmart",20,2,10);
+                    objEmpWageForMulCompany.computeEmpWage("Reliance", 10, 4, 20);
+                    objEmpWageForMulCompany.computeEmpWage("Vodafone", 50, 5, 40);
+                    objEmpWageForMulCompany.computeEmpWage("TaTa", 100, 6, 60);
+                    break;
+
+
+                case 0:
+                    return;
+
+                default:
+                    Console.WriteLine("**\n Please Enter Correct Choice**");
+                    break;
+
+
+
+            }
+        }
+    }
+}
+
+
+            
+
