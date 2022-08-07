3b9e13354051be6cc072d16f885ad198c2499b4e
bhumika1111 <bhoyarpatil@434>
2022-08-07 17:38:47 +0530

    * Check Employee Presence *

---
+++ EmployeePresence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageComputation
+{
+    /// <summary>
+    /// Here We Check Only Employee Is Present Or Absent By Using Random Function.
+    /// </summary>
+    public class EmployeePresence
+    {
+        /// <summary>
+        /// Checks the employee attendance.
+        /// </summary>
+        public  void CheckEmpPresence()
+        {
+
+            int IS_FUll_TIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FUll_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+
+   }
+
+   
+        
+    
+
+}
